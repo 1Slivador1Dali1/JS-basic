5a81b0a6d8c7c808bf1be73154a19cebf3068a20
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-04-29 23:17:23 +0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello World')
+let a = 5;
+let b = 5.6;
+console.log(typeof a);
+console.log(typeof b);
+
+a = 'string';
+console.log(typeof a);
+
+let isAdmin = false;
+console.log(typeof isAdmin);
