3bf7105d147a15b08137c694a4df92aa36e7163d
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-04-30 21:07:25 +0300

    21 ex

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const payRateUsd = 80;
-const projectHours = 40;
-const availableHours = (11 - 2) * 5;
+// const payRateUsd = 80;
+// const projectHours = 40;
+// const availableHours = (11 - 2) * 5;
 
-console.log('Смогу ли я работать? ' + (availableHours > projectHours));
-console.log('Соклько я хочу получить? ' + (payRateUsd * projectHours))
+// console.log('Смогу ли я работать? ' + (availableHours > projectHours));
+// console.log('Соклько я хочу получить? ' + (payRateUsd * projectHours))
+
+const allSum = 12000;
+const oneYeInMo = 24;
+const dep = 0.07;
+const priceHouse = 13500;
+
+const itog = allSum * (1 + dep / 12) ** oneYeInMo;
+
+if (itog > priceHouse) {
+    console.log('Может купить ' + 'Остаток: ' + (itog - priceHouse));
+}
+else {
+    console.log('Не сможет купить');
+}
