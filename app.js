3fe87589bc14794517261b775dec431ca6fffd41
1Slivador1Dali1 <vladislav.lashkov64@gmail.com>
2024-04-30 22:56:20 +0300

    25 ex

--- app.js
+++ app.js
@@ -5,16 +5,29 @@
 // console.log('Смогу ли я работать? ' + (availableHours > projectHours));
 // console.log('Соклько я хочу получить? ' + (payRateUsd * projectHours))
 
-const allSum = 12000;
-const oneYeInMo = 24;
-const dep = 0.07;
-const priceHouse = 13500;
+// const allSum = 12000;
+// const oneYeInMo = 24;
+// const dep = 0.07;
+// const priceHouse = 13500;
 
-const itog = allSum * (1 + dep / 12) ** oneYeInMo;
+// const itog = allSum * (1 + dep / 12) ** oneYeInMo;
 
-if (itog > priceHouse) {
-    console.log('Может купить ' + 'Остаток: ' + (itog - priceHouse));
-}
-else {
-    console.log('Не сможет купить');
+// if (itog > priceHouse) {
+//     console.log('Может купить ' + 'Остаток: ' + (itog - priceHouse));
+// }
+// else {
+//     console.log('Не сможет купить');
+// }
+
+const vvediet = prompt('7 + или - 15');
+
+switch (true) {
+    case Number(vvediet) === 22:
+    case Number(vvediet) === -8:
+    case vvediet === 'Я не робот':
+        console.log('Успех');
+        break;
+    default:
+        console.log('Вы робот!');
+        break;
 }
